b840d71df3f4651fbc123fe90830621b67027d94
ANPATIL\arunp <arun>
2023-09-21 20:15:19 +0530

    added

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_instance" {
     ami = "ami-03f65b8614a860c29"
-    instance_type = "t2.micro"
-    count = "4"
+    instance_type = "var.instance_type"
+    count = "1"
     tags = {
     Name = "my_first_instance"
   }
